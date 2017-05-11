5cd931172e76bcdb08c9306934f574156f51291b
restress <u201411142@hust.edu.cn>
2017-05-11 16:19:04 +0800

    BeatBox
    SoundPool&style&drawable&Asset

---
+++ BeatBox/app/src/main/java/com/example/restress/beatbox/Sound.java
@@ -0,0 +1,34 @@
+package com.example.restress.beatbox;
+
+/**
+ * Created by restress on 2017/5/10.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    public Sound(String assetPath){
+        mAssetPath =assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length-1];
+        mName =filename.replace(".wav","");
+    }
+
+    public String getAssetPath(){
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+}
